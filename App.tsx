31ace871686b6b4af6b9bb31078014277e7bcc28
Munnawar Hussain <munawwarh48@.com>
2024-07-19 18:24:22 +0530

    navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Homescreen from './Components/Homescreen'
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import React from 'react';
+import Login from './Components/Login';
+import Homescreen from './Components/Homescreen';
+import Signup from './Components/Signup';
+
 
 export default function App() {
   return (
-    <View>
-      <SafeAreaView>
-        <Homescreen />
-      </SafeAreaView>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <View style={styles.parentContainer}>
+        <Homescreen></Homescreen>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  parentContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
--- Components/Homescreen.tsx
+++ Components/Homescreen.tsx
@@ -1,64 +1,79 @@
-import {StyleSheet,Text,Linking,TouchableOpacity, View, Image,} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
 import React from 'react';
 
 export default function Homescreen() {
   return (
-<View>
-      {/* Background image and logo*/}
-      <View>
-        <Image style={styles.backgroundImage} source={require('../assets/bg.jpg')} />
-        {/* Logo  */}
+    <SafeAreaView style={styles.parentContainer}>
+      {/* Container 1 for background image and logo */}
+      <View style={styles.container1}>
+        <Image
+          style={styles.backgroundImage}
+          source={require('../assets/bg.jpg')}
+        />
+        {/* Logo */}
         <View style={styles.logoContainer}>
-          <Image style={styles.logoImage} source={require('../assets/logo.png')} />
+          <Image
+            style={styles.logoImage}
+            source={require('../assets/logo.png')}
+          />
         </View>
       </View>
 
-      {/* Buttons Container  */}
-    <View style={styles.buttonContainer}>
-        {/* Login Button  */}
-        <TouchableOpacity  activeOpacity={0.6}  style={styles.button}>
+      {/* Container 2 for login and signup buttons */}
+      <View style={styles.container2}>
+        {/* Login Button */}
+        <TouchableOpacity activeOpacity={0.6} style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
-        {/* Sign Up Button  */}
-        <TouchableOpacity  activeOpacity={0.6}  style={styles.button2}>
+        {/* Sign Up Button */}
+        <TouchableOpacity activeOpacity={0.6} style={styles.button2}>
           <Text style={styles.button2Text}>Sign Up</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        height: 450,
-        width: 400,
-      },
-      logoImage: {
-        width: '70%', 
-        height: '70%', 
-        resizeMode: 'contain',
-      },
-      logoContainer: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-  buttonContainer: {
+  parentContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container1: {
+    height: '60%',
+    width: '100%',
+  },
+  container2: {
+    height: '40%',
+    width: '100%',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 130,
-   
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  logoImage: {
+    width: '70%',
+    height: '70%',
+    resizeMode: 'contain',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     backgroundColor: '#8F1413',
-    padding: 8,
-    height:45,
+    padding: 10,
+    height: 45,
     width: 170,
     borderRadius: 20,
     marginTop: 20,
@@ -72,8 +87,8 @@ const styles = StyleSheet.create({
   },
   button2: {
     backgroundColor: '#FFDC52FF',
-    padding: 8,
-    height:45,
+    padding: 10,
+    height: 45,
     width: 170,
     borderRadius: 20,
     marginTop: 20,
