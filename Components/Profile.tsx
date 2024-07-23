3fda23dca2107c0c6aba90aefefa0f9667fdac56
Munnawar Hussain <munawwarh48@.com>
2024-07-23 12:52:18 +0530

    latest

--- Components/Profile.tsx
+++ Components/Profile.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Profile() {
+const = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+export default
